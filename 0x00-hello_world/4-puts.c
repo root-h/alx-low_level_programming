11c07151ee9150a3c19db390e76591cc3d65cde5
root-h <hannahmxo1.hm@gmail.com>
2022-03-10 19:54:30 +0000

    helloputs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is how to describe a function betty style
+ * return:  puss
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+	{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	}
