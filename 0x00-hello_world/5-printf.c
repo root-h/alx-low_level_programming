a3eb9d68dd35cb9cfb3cba8e9efcb5e62bd423f2
root-h <hannahmxo1.hm@gmail.com>
2022-03-10 20:08:22 +0000

    helloprintf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Short description, single line
- * @parameterx: Description of parameter x
+ * Description of parameter x
 (* a blank line
  * Description: Longer description of the function)?
 (* section header: Section description)*
@@ -9,6 +9,6 @@
  */
 int main(void)
 	{
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 	}
