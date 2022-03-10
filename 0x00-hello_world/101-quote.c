f73ce113c21f85ae65cf1f681ebbdfc19a0acff4
root-h <hannahmxo1.hm@gmail.com>
2022-03-10 21:15:39 +0000

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ * Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void);
+	{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+	}
