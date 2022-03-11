3a9a239ed1c348f6d3227b3ff16c0232c330bcbd
root-h <hannahmxo1.hm@gmail.com>
2022-03-11 17:34:32 +0000

    reversealph

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Printing the alphabet in reverse mode.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
