6e2bee275de7e550ec013eaf5e907a708253c051
root-h <hannahmxo1.hm@gmail.com>
2022-03-11 17:59:51 +0000

    singledcombo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Put all of the possible combinations of single digit numbers.
+(*
+ * Return: 0 when on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+
+}
