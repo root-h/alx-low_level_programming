31dbe6381e8263ccfb5359f4d6d8404e17c0fc64
root-h <hannahmxo1.hm@gmail.com>
2022-03-11 17:03:26 +0000

    lowercasealph

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - echoes the alphabet in lowercase on the screen.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
