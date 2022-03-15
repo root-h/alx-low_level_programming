9fc0d3f17c3880c08d71639819ec237a5f843552
root-h <hannahmxo1.hm@gmail.com>
2022-03-15 22:52:52 +0000

    add2num

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - two numbers together.
+ * @y: The first number to be added.
+ * @h: The second number to be added.
+ * Return: sum the two numbers.
+ */
+int add(int y, int h)
+{
+return (y + h);
+}
