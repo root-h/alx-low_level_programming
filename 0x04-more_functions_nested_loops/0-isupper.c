66d27d48ca2d08a8997b18a02ca7999cb6817e84
root-h <hannahmxo1.hm@gmail.com>
2022-03-17 08:00:09 -0700

    uppercasealph

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *_isupper - check if a character is an uppercase in English alphabet
+ *_isupper - check if a character is an
+ *uppercase in English alphabet
  * Return: 1 if it is uppercase letter , 0 if it not
  *@c : character to test
  */
