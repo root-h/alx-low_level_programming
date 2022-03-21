7fae5b8354df5993b96cb3d9bf3844c4f2a501db
root-h <hannahmxo1.hm@gmail.com>
2022-03-21 23:29:03 +0000

    convertstrint

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,23 +8,39 @@
  */
 int main(void)
 {
-  int n;
+int i, d, n, len, f, digit;
 
-  n = _atoi("98");
-  printf("%d\n", n);
-  n = _atoi("-402");
-  printf("%d\n", n);
-  n = _atoi("          ------++++++-----+++++--98");
-  printf("%d\n", n);
-  n = _atoi("214748364");
-  printf("%d\n", n);
-  n = _atoi("0");
-  printf("%d\n", n);
-  n = _atoi("Suite 402");
-  printf("%d\n", n);
-  n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-  printf("%d\n", n);
-  n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-  printf("%d\n", n);
-  return (0);
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
