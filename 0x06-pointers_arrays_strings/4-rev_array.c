28f38ac5534588a9f35f2b7f2e11280325322554
root-h <hannahmxo1.hm@gmail.com>
2022-03-24 20:07:05 +0000

    revarray

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * reverse_array - reversing the content of an array of integers.
+ * @a: the array to reverse
+ * @n: size of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	i = tmp =  0;
+	n -= 1;
+	while (i <= n)
+	{
+		tmp = a[n];
+		a[n--] = a[i];
+		a[i++] = tmp;
+	}
+}
