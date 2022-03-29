361a0658a9a660a955b8c8a8f34ea11f282adf15
root-h <hannahmxo1.hm@gmail.com>
2022-03-29 05:52:34 -0400

    set-string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - setting the pointer value to a char.
+ * @s: The pointer itself.
+ * @to: The char itself.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
