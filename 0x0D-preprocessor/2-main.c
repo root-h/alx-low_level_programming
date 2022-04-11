0507cca0ab9b5f2bc5ff3cf79a2fdacfb6e97751
root-h <hannahmxo1.hm@gmail.com>
2022-04-11 19:36:42 +0000

    macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - printing file when it is compiled from
+  * @void: nothing
+  * Return: 0 on success
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
