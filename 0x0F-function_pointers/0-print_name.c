85c2e519e955ba42b8f655722d4c246cd59ee161
root-h <hannahmxo1.hm@gmail.com>
2022-04-13 22:31:26 +0000

    fnprintsname

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,13 @@
 #include "function_pointers.h"
 /**
   * print_name - printing the name
-  * @name: inputing string
-  * @f: function pointer casted to char pointer
+  * @name: name to print
+  * @f: pointer to a function to print a name
   */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
