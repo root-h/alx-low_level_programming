b0a11acf9508376be3d2115ecac54d15f8731c87
root-h <hannahmxo1.hm@gmail.com>
2022-04-26 22:57:40 +0300

    nodesinlist

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+  * listint_len - getting the number of nodes in a list
+  * @h: singly linked list to print
+  * Return: nodes number in the list
+  */
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
