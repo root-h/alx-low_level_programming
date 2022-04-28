c90d6edd97f010d62631344ab06205cd1145a426
root-h <hannahmxo1.hm@gmail.com>
2022-04-29 00:26:17 +0300

    bitmanipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * get_endianness - Checks the endianness.
- *
- * Return: If big-endian - 0.
- *         If little-endian - 1.
- */
+  * get_endianness - returning processor endianness
+  * Return: 0 if big endian, 1 if little endian
+  */
 int get_endianness(void)
 {
-	int num = 1;
-	char *endian = (char *)&num;
+	int i;
+	char *test;
 
-	if (*endian == 1)
-		return (1);
-
-	return (0);
+	i = 1;
+	test = (char *)&i;
+	return ((int)test[0]);
 }
